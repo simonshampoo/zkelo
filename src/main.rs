c086198b44ac40ec8eee2606e44a4da63e865494
Simon Shamoon <simonshm5@gmail.com>
2022-11-29 21:26:39 -0600

    ugh

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use elo::{calculate_Ea, calculate_Eb};
+use secrets::Secret;
+/*
 
+I assume that players within a range of +-100 of each other is fair rating.
+
+Maybe we could introduce custom ranges to indicate fair pairings but I think 100 is sufficient.
+*/
 fn main() {
-    println!("{}% chance of winning", calculate_Ea(100, 1200));
+    Secret::<[u8; 16]>::random(|s| {
+        println!("{:?}", s);
+    })
 }
